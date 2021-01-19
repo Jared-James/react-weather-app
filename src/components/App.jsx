d031648286359d88a7eaa7bf50d2234b72961590
jared james <jaredjames.dev@gmail.com>
2021-01-19 18:47:44 +1300

    started project

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../../styles/styles.scss"
+
+const App = () => (
+    <div>
+    <h1>This is a webpack, babel, sass, react application</h1>
+    </div>
+)
+
+export default App
