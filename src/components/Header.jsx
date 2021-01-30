7c4cac56d3b7fb12ace8aa19290d36cccdb7de4e
jared james <jaredjames.personal@gmail.com>
2021-01-30 13:27:25 +1300

    added to header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ const Header = () => (
     <>
         <div id="title">
             <h1>NZ Local Weather</h1>
+            <p>Please add country code for accuracy of location</p>
         </div>
     </>
 )
