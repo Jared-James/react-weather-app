f5cd238da669cbdfd027888b1ae23280a9d4ffbe
jared james <jaredjames.personal@gmail.com>
2021-02-25 13:22:34 +1300

    added subtitle and header to main component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,8 @@ import React from 'react'
 const Header = () => (
     <>
         <div id="title">
-            <h1>NZ Local Weather</h1>
-            <p>Please add country code for accuracy of location</p>
-            <p>Example: Auckland NZ</p>
+            <h1>NewZealand Local Weather</h1>
+           
         </div>
     </>
 )
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,7 +4,14 @@ const Loading = () => {
     return (
         <>
             <div id="day1">
-                <h1>Feel free to search a location!</h1>
+                <div id="titleText">
+                    <h1>Please enter in a location</h1>
+                    <div id="titleTextSubtitle">
+                        <p>Please add country code for accuracy of location</p>
+                        <p>Example: Auckland NZ</p>
+                    </div>
+                </div>
+
             </div>
         </>
     )
