2370c12716c1f032f973fb6177c62a0d0fb4d241
jared james <jaredjames.personal@gmail.com>
2021-02-25 14:55:27 +1300

    making mobile look like

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 const Header = () => (
     <>
         <div id="title">
-            <h1>NewZealand Local Weather</h1>
-           
+            <div id="titleText">
+                <h1>NewZealand Local Weather</h1>
+            </div>
+
         </div>
     </>
 )
